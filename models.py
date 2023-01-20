889f7b1b169d995ad76ad177b186d4505299248b
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-01-20 14:33:42 -0500

    Added account and transaction classes

--- models.py
+++ models.py
@@ -3,12 +3,31 @@ from app import db
 
 
 class User(UserMixin, db.Model):
-    __table__name = "user"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    pwd = db.Column(db.String(300), unique=True, nullable=False)
+    password = db.Column(db.String(300), unique=True, nullable=False)
 
-    def __repr__(self):
-        return f'<Uerson {self.username}>'
+class Account(db.Model):
+    __tablename__ = 'accounts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref=db.backref('users', lazy=True))
+    name = db.Column(db.String(80), nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    balance = db.Column(db.Float, nullable=False)
+
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
+    account = db.relationship(
+        'Account', backref=db.backref('accounts', lazy=True))
+    date = db.Column(db.Date, nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(120))

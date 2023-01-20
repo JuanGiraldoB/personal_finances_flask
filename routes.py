06666ced07445a1436a63f7dd27a5a89ff9234ef
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-01-20 18:05:02 -0500

    Created create_wallet route and index now redirects to this route when an user is authenticaed

--- routes.py
+++ routes.py
@@ -4,7 +4,8 @@ from flask import (
     redirect,
     flash,
     url_for,
-    session
+    session,
+    request
 )
 
 from datetime import timedelta
@@ -30,8 +31,8 @@ from flask_login import (
 )
 
 from app import create_app, db, login_manager, bcrypt
-from models import User
-from forms import login_form, register_form
+from models import User, Account
+from forms import login_form, register_form, account_creation
 
 # Used by Flask-Login to load the user from the database when it is neeeded
 # for example, when a user request a page that requires authentication
@@ -46,7 +47,39 @@ app = create_app()
 # Home route
 @app.route("/", methods=("GET", "POST"), strict_slashes=False)
 def index():
+    # TODO: if the user has an account dont send to new_user.html
+    if current_user.is_authenticated:
+        return redirect(url_for("create_wallet"))
     return render_template("index.html", title="Home")
+
+@app.route("/create_wallet/", methods=("GET", "POST"), strict_slashes=False)
+@login_required
+def create_wallet():
+    form = account_creation()
+
+    if form.validate_on_submit():
+        try:
+            acc_name = form.name.data
+            acc_initial_balance = form.initial_balance.data
+            acc_type = form.type.data
+            acc_user_id = current_user.id
+
+            new_account = Account(
+                user_id=acc_user_id,
+                name=acc_name,
+                type=acc_type,
+                balance=acc_initial_balance
+            )
+
+            db.session.add(new_account)
+            db.session.commit()
+            flash(f"Wallet Succesfully created", "success")
+
+        except:
+            pass
+
+    name_current_user = current_user.name
+    return render_template("new_user.html", title=f'Welcome {name_current_user}', form=form)
 
 # Login route
 @app.route("/login/", methods=("GET", "POST"), strict_slashes=False)
@@ -60,11 +93,11 @@ def login():
                 login_user(user)
                 return redirect(url_for('index'))
             else:
-                flash("Invalid Username or password!", "danger")
+                flash("Invalid name or password!", "danger")
         except Exception as e:
             flash(e, "danger")
 
-    return render_template("auth.html", form=form)
+    return render_template("auth.html", form=form, btn_action="Login")
 
 # Register route
 @app.route("/register/", methods=("GET", "POST"), strict_slashes=False)
@@ -75,10 +108,10 @@ def register():
         try:
             email = form.email.data
             password = form.password.data
-            username = form.username.data
+            name = form.name.data
 
             new_user = User(
-                username=username,
+                name=name,
                 email=email,
                 password=bcrypt.generate_password_hash(password).decode('utf8'),
             )
@@ -91,13 +124,14 @@ def register():
         except Exception as e:
             flash(e, "danger")
 
-    return render_template("auth.html", form=form)
+    return render_template("auth.html", form=form, btn_action="Register")
 
 # Logout route
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
+
     return redirect(url_for('login'))
 
 

a6555a2ff096b1dda2503a8ddd683aff09b0b994
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-01-23 19:03:52 -0500

    Added delete on cascade

--- models.py
+++ models.py
@@ -10,23 +10,24 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), unique=True, nullable=False)
 
+
 class Account(db.Model):
     __tablename__ = 'accounts'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user = db.relationship('User', backref=db.backref('users', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref('users', lazy=True, cascade="all, delete-orphan", passive_deletes=True))
     name = db.Column(db.String(80), nullable=False)
-    type = db.Column(db.String(20), nullable=False)
     balance = db.Column(db.Float, nullable=False)
+
 
 class Transaction(db.Model):
     __tablename__ = 'transactions'
 
     id = db.Column(db.Integer, primary_key=True)
-    account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
+    account_id = db.Column(db.Integer, db.ForeignKey('accounts.id', ondelete='CASCADE'))
     account = db.relationship(
-        'Account', backref=db.backref('accounts', lazy=True))
+        'Account', backref=db.backref('accounts', lazy=True, cascade="all, delete-orphan", passive_deletes=True))
     date = db.Column(db.Date, nullable=False)
     type = db.Column(db.String(20), nullable=True)
     amount = db.Column(db.Float, nullable=False)

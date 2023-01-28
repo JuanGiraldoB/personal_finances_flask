ef6cca321b54cc5fda7a82104e7bcf81973b5c8b
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-01-28 15:27:37 -0500

    deleted unused imports

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 import configparser as cp
 
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    current_user,
-    logout_user,
-    login_required,
-)
+from flask_login import LoginManager
 
 login_manager = LoginManager()
 
@@ -32,7 +25,7 @@ migrate = Migrate()
 bcrypt = Bcrypt()
 
 
-def create_app() -> Flask:
+def create_app():
     config = cp.ConfigParser()
     config.read('config.ini')
 

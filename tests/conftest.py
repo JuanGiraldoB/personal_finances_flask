4c9d06cac908c1bf74282988f9c9f0259c03fd4d
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-01-30 09:07:22 -0500

    Initial commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,12 @@
 import pytest
-# from app import create_app, db
-from app import create_app
+from project import create_app, db
 
 @pytest.fixture()
 def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+    app = create_app(testing=True)
+
+    with app.app_context():
+        db.create_all()
 
     yield app
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,26 @@
-def test_request_example(client):
-    response = client.get("/dashboard")
-    assert response.status_code == 200
+from project.models import User
+
+
+class TestRoutes:
+    def test_index(self, client):
+        response = client.get("/")
+        
+        assert response.status_code == 200
+        assert b"Personal Finances" in response.data 
+
+    def test_registration(self, client, app):
+        data = {
+            "name": "testing",
+            "email": "testing@testing.com",
+            "password": "testing105369845/",
+            "verify_password": "testing105369845/"
+        }
+
+        response = client.post("/register/", data=data)
+
+        assert response.status_code == 302
+
+        with app.app_context():
+            assert User.query.count() == 1
+            assert User.query.first().email == data["email"]
+
